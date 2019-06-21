5aea3a17546f8a9615f45680079992db08f01f6c
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2019-06-21 12:45:25 +0200

    Fixed several bugs in the script

--- run.py
+++ run.py
@@ -1,64 +1,60 @@
 import argparse
 import sys
+import re
 import tensorflow as tf
 import numpy as np
 
-# TODO:
-# File naming if not provided by the user the default could simply be the input file name stripped of its extension + "_ObBmIb_bounds" + .txt
-
-
-NUM_LAYERS = 50
+NUM_INTENSITY_VALUES = 50
+MAX_INTENSITY_VALUE = 255
 
 parser = argparse.ArgumentParser(
-    description="Make predictions for start and end of bone marrow",
+    description="Run a Keras model to identify layer boundaries based on a "
+                "vector of intensities.",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 parser.add_argument("--model", default="model.h5",
         help="Model file to use in Keras HDF5 format")
-parser.add_argument("--input-norm", default=255, type=float,
-        help="Normalisation constant for inputs (intensities)")
 parser.add_argument("--delimiter", default=";",
         help="Field delimiter used in the program's input and output")
 parser.add_argument("--output-file", default=None,
         help="Output file name. Default is INPUTNAME_ObBmIb_bounds.txt, "
              "where INPUTNAME is the input file name without extension.")
 parser.add_argument("INPUT",
-        help="Input file containing the intensity values, read from "
-             "standard input if not given.")
+        help="Input file containing the intensity values.")
 
 args = parser.parse_args()
 
 def load_input_and_orient(input_file):
-    np.loadtxt(input_file, delimiter=args.delimiter)
-    transposed = False
-    if data.shape[1] != NUM_LAYERS:
-        if data.shape[0] == NUM_LAYERS:
+    data = np.loadtxt(input_file, delimiter=args.delimiter)
+    is_transposed = False
+    if data.shape[1] != NUM_INTENSITY_VALUES:
+        if data.shape[0] == NUM_INTENSITY_VALUES:
             data = np.transpose(data)
-            transposed = True
+            is_transposed = True
         else:
             print("Error: The input data has dimensions {}, but expected to have {}"
                     " intensity values per observation.".format(data.shape,
-                        NUM_LAYERS)
+                        NUM_INTENSITY_VALUES)
                     )
             sys.exit(1)
-    return data, transposed
+    return data, is_transposed
 
 def main():
     model = tf.keras.models.load_model(args.model)
-    data, transposed = load_input_and_orient(args.INPUT)
+    data, is_transposed = load_input_and_orient(args.INPUT)
 
-    # TODO: Change the normalisation constant to max of all the current inputs?
-    norm_inputs = data / args.input_norm
+    norm_inputs = data / MAX_INTENSITY_VALUE
     predictions = model.predict(norm_inputs)
-    rounded_predictions = np.round(predictions * args.output_norm)
-    predictions = np.clip(predictions, a_min=1, a_max=NUM_LAYERS, out=predictions)
+    predictions *= NUM_INTENSITY_VALUES
+    predictions = np.clip(predictions, a_min=1, a_max=NUM_INTENSITY_VALUES,
+                            out=predictions)
     num_predictions = len(predictions)
-    if transposed:
+    if is_transposed:
         predictions = np.transpose(predictions)
     if not args.output_file:
-        args.output_file = re.sub(args.INPUT, r"(\.\w+)?$", "_ObBmIb_bounds.txt")
+        args.output_file = re.sub(r"(\.\w+)?$", "_ObBmIb_bounds.txt", args.INPUT)
     np.savetxt(args.output_file, predictions, fmt="%1.0f", delimiter=args.delimiter)
-    print("{} predictions written to file {}.".format(
+    print("{} results written to file {}.".format(
         num_predictions,
         args.output_file
         ))
